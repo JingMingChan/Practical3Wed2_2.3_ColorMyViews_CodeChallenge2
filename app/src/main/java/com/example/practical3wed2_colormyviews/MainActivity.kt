50422113e331dcd854a4fc743f439d33435a4e5f
JingMingChan <prochan75103161@gmail.com>
2019-11-11 20:36:21 +0800

    color my view(fill entire layout) ver 2

--- app/src/main/java/com/example/practical3wed2_colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/practical3wed2_colormyviews/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
 
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText: ImageView = findViewById(R.id.box_two_text)
+        val boxTwoText= findViewById<TextView>(R.id.box_two_text)
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundResource(R.drawable.user)
+            R.id.box_two_text -> view.setBackgroundColor(Color.YELLOW)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
